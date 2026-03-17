0b728475c702a72a2877238fff5b63843141efe3
Lohit_Sathesh <slohitashwa@gmail.com>
2026-03-17 09:10:25 +0530

    UC2 complete

---
+++ BookMyStay/src/DoubleRoom.java
@@ -0,0 +1,10 @@
+/**
+ * CLASS - DoubleRoom
+ * Represents a double room in the hotel.
+ * @version 2.1
+ */
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
---
+++ BookMyStay/src/SingleRoom.java
@@ -0,0 +1,10 @@
+/**
+ * CLASS - SingleRoom
+ * Represents a single room in the hotel.
+ * @version 2.1
+ */
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
